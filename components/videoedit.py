6ffdb67b0cd7abf9009c19cf8807bf33e6ee249d
yizhz <zhouying1996@hotmail.com>
2022-05-23 09:37:54 +0800

    first commit

---
+++ components/videoedit.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+
+import abc
+import os
+import tempfile
+
+import suanpan
+from suanpan import asyncio, error, g
+from suanpan import path as spath
+from suanpan import runtime
+from suanpan.app import app
+from suanpan.app.arguments import Json, String, Int
+from suanpan.log import logger
+from suanpan.node import node
+from suanpan.storage import storage
+
+
+from moviepy.editor import *
+
+
+@app.afterInit
+def initFront(_):
+    app.modules.front.init("statics/editor")
+
+
+def videoEdit(args):
+    # args = context.args
+    infile=args['inFile'][0]
+    outfile=args['saveFile']
+    subclip_start=args['subclipStart']
+    subclip_end=args['subclipEnd']
+    clip = VideoFileClip(infile).subclip(subclip_start,subclip_end)
+    
+    clip.write_videofile(outfile)
+    app.send({"out1": outfile})
+
+if __name__ == "__main__":
+    suanpan.run(app)
